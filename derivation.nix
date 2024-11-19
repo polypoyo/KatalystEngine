0ec2aa6b9d1f0dc007bf80567b0f94bd198ce290
hyperboid <23138363+polypoyo@users.noreply.github.com>
2024-11-19 04:45:39 -0500

    Add Install targets

--- derivation.nix
+++ derivation.nix
@@ -17,12 +17,6 @@ in
     nativeBuildInputs = with pkgs; [staticSDL2 staticSDL2.dev cmake vcpkg ninja ];
     buildInputs = with pkgs; [ spdlog glfw ];
 
-    # TODO: Fix install target so this actually works
-    fixupPhase = ":"; # does nothing. normally autopatchelf panics.
-    installPhase = ''
-      cp -r . $out
-    '';
-
     meta = {
       mainProgram = "katalyst";
       platforms = lib.platforms.linux;
