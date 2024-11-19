41dd67b40b0be5df31e097f43f3259cad202e9b9
hyperboid <23138363+polypoyo@users.noreply.github.com>
2024-11-19 04:57:48 -0500

    Nix: Don't depend on SDL Based this off of a different package I was working on and forgot to remove.

--- derivation.nix
+++ derivation.nix
@@ -3,18 +3,13 @@
   stdenv,
   lib,
   ...
-}: let
-  staticSDL2 = pkgs.SDL2.overrideAttrs (old: {
-    dontDisableStatic = true;
-  });
-in
-  stdenv.mkDerivation {
+}: stdenv.mkDerivation {
     pname = "katalyst";
     version = "0.0";
 
     src = ./.;
 
-    nativeBuildInputs = with pkgs; [staticSDL2 staticSDL2.dev cmake vcpkg ninja ];
+    nativeBuildInputs = with pkgs; [ cmake vcpkg ninja ];
     buildInputs = with pkgs; [ spdlog glfw ];
 
     meta = {
