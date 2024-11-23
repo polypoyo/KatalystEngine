6dd89478c9a16c4fef2ed421f1dd91448986f3f1
hyperboid <23138363+polypoyo@users.noreply.github.com>
2024-11-23 10:22:03 -0500

    Nix: Pin Nixpkgs version

--- default.nix
+++ default.nix
@@ -1,4 +1,4 @@
 let
-  pkgs = import <nixpkgs> { };
+  pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/c26faf0c26de27cd38468fb25b2275765d78d087.tar.gz") { };
 in
 pkgs.callPackage ./derivation.nix { }
